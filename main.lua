164aa7d8b1d5096747f14a49a54b25f36c71513c
AviansEpic <64453220+AviansEpic@users.noreply.github.com>
2021-09-21 14:12:47 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,15 @@
-return "penis"
+local api = {}
+
+function api:DownloadFromUrl(filetosave, url)
+  writefile(filetosave, game:HttpGet((url), true))
+end
+
+function api:CreateFileIfDoesntExist(name, contents)
+ if not isfile(name) then
+    writefile(name, contents)
+    return true
+ end
+  return false
+end
+
+return api
