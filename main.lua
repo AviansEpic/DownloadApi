e2bd2aeff027154ba668785aea4be60c9eb94bed
AviansEpic <64453220+AviansEpic@users.noreply.github.com>
2021-09-21 14:05:51 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,1 @@
-local api = {}
-
-api:DownloadFromUrl(name, url)
-  writefile(name, game:HttpGet(url))
-end
-
-api:DuplicateFile(filename, newname)
-  writefile(newname, readfile(filename))
-end)
-
-return api
+return "penis"
